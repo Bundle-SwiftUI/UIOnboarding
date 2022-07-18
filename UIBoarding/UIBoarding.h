c0023cb5db0445cd512cc3e661662c7bdd868697
Alok Choudhary <alok.c83@gmail.com>
2022-07-18 15:11:19 -0500

    Framework

---
+++ UIBoarding/UIBoarding.h
@@ -0,0 +1,18 @@
+//
+//  UIBoarding.h
+//  UIBoarding
+//
+//  Created by Alok Choudhary on 7/18/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIBoarding.
+FOUNDATION_EXPORT double UIBoardingVersionNumber;
+
+//! Project version string for UIBoarding.
+FOUNDATION_EXPORT const unsigned char UIBoardingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIBoarding/PublicHeader.h>
+
+
